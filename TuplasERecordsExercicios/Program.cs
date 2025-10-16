574e1d7536b864281bd9345dc0464ab5cfd1710a
Alessandro Fernandes <131491254+Alefernandxx@users.noreply.github.com>
2025-10-16 16:33:12 -0300

    Add files via upload

---
+++ TuplasERecordsExercicios/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args) //Execução
+    {
+        Console.WriteLine("O analista de Numeros");
+
+        var listaDeNumeros = new List<int> { 5, 1, 9, 0, 7, -2 };
+
+        var resultado = AnalisarNumeros(listaDeNumeros);
+
+        Console.WriteLine($"O menor valor é {resultado.menorValor}");
+        Console.WriteLine($"O maior valor é {resultado.maiorValor}");
+    }
+
+    public static (int menorValor, int maiorValor) AnalisarNumeros(List<int> numeros) // código para encontrar valores.
+    {
+        int menorValor = numeros[0];
+        int maiorValor = numeros[0];
+
+        foreach (int numero in numeros)
+        {
+            if (numero < menorValor)
+            {
+                menorValor = numero;
+            }
+            if (numero > maiorValor)
+            {
+                maiorValor = numero;
+            }
+
+        }
+        return (menorValor, maiorValor);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
